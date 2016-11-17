adbf037457135367bb264a90d511676cd4cda27c
barlowm <barlowm@gmail.com>
2016-11-16 23:03:57 -0500

    Initial commit

---
+++ app/view/main/Main.js
@@ -0,0 +1,9 @@
+Ext.define('VistA.Surgery.view.main.Main', {
+    extend: 'Ext.panel.Panel',
+    items: [
+        {
+            title: 'Employee Directory',
+            html: "<h1>Hello World</h1>"
+        }
+    ]
+});
